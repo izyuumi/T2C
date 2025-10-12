4f3895d77a961b76f7ddfa2aad5110df387e7932
Yumi Izumi <mail@yumi.to>
2025-10-12 14:04:28 +0900

    chore: initial commit

---
+++ T2C/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  T2C
+//
+//  Created by Yumi Izumi on 2025/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ T2C/T2CApp.swift
@@ -0,0 +1,17 @@
+//
+//  T2CApp.swift
+//  T2C
+//
+//  Created by Yumi Izumi on 2025/10/12.
+//
+
+import SwiftUI
+
+@main
+struct T2CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
